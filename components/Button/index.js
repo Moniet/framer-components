7e7f764fb62bc3646befe6dba9e2335f38158ed0
Moniet <moniet.sawhney@gmail.com>
2023-04-23 00:06:20 +0200

    Updates

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,4 +1,4 @@
-// esbuild-css-modules-plugin-namespace:/var/folders/07/kf6jf5m12g53trp7qpjtjg1m0000gn/T/tmp-94563-oX0nODKse4M9/code-components/src/components/Button/Button.modules.css.js
+// esbuild-css-modules-plugin-namespace:/var/folders/07/kf6jf5m12g53trp7qpjtjg1m0000gn/T/tmp-98128-teToeFNLG6V9/code-components/src/components/Button/Button.modules.css.js
 var digest = "0e00bb3b212d3c51ff9d50e5dc5d86f0da538e2ca2a86029f58d7ca7c9c2071f";
 var css = `._btn_12qt7_1 {
   background: #222;
@@ -7,7 +7,7 @@ var css = `._btn_12qt7_1 {
 }
 `;
 (function() {
-  if (global.window) {
+  if (globalThis.window) {
     if (!document.getElementById(digest)) {
       var ele = document.createElement("style");
       ele.id = digest;
