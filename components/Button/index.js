02a42618a0e1b08548c0f1ee0239f21e835d1a4c
Moniet <moniet.sawhney@gmail.com>
2023-04-22 23:56:02 +0200

    Updates

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,20 +1,22 @@
-// esbuild-css-modules-plugin-namespace:/var/folders/07/kf6jf5m12g53trp7qpjtjg1m0000gn/T/tmp-90076-PCMpaDwa8I3m/code-components/src/components/Button/Button.modules.css.js
+// esbuild-css-modules-plugin-namespace:/var/folders/07/kf6jf5m12g53trp7qpjtjg1m0000gn/T/tmp-94563-oX0nODKse4M9/code-components/src/components/Button/Button.modules.css.js
 var digest = "0e00bb3b212d3c51ff9d50e5dc5d86f0da538e2ca2a86029f58d7ca7c9c2071f";
-var css = `._button_13ozy_1 {
+var css = `._btn_12qt7_1 {
   background: #222;
   color: white;
   border-radius: 10px;
 }
 `;
 (function() {
-  if (!document.getElementById(digest)) {
-    var ele = document.createElement("style");
-    ele.id = digest;
-    ele.textContent = css;
-    document.head.appendChild(ele);
+  if (global.window) {
+    if (!document.getElementById(digest)) {
+      var ele = document.createElement("style");
+      ele.id = digest;
+      ele.textContent = css;
+      document.head.appendChild(ele);
+    }
   }
 })();
-var Button_modules_css_default = { "button": "_button_13ozy_1" };
+var Button_modules_css_default = { "btn": "_btn_12qt7_1" };
 
 // src/components/Button/index.tsx
 import { jsx } from "react/jsx-runtime";
