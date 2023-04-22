b75e0045f6254ded341d80fef137fd40e926329d
Moniet <moniet.sawhney@gmail.com>
2023-04-22 17:10:06 +0200

    Updates

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,13 +1,14 @@
 // src/components/Button/index.tsx
 var styles = {
   padding: "1rem",
-  borderRadius: " 10px",
+  borderRadius: "10px",
   background: "#222",
   color: "#f2f2f2"
 };
 var Button = () => {
   return <div style={styles}>Button Text</div>;
 };
+var Button_default = Button;
 export {
-  Button
+  Button_default as default
 };
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-// src/components/Button/index.tsx
-var styles = {
-  padding: "1rem",
-  borderRadius: " 10px",
-  background: "#222",
-  color: "#f2f2f2"
-};
-var Button = () => {
-  return <div style={styles}>Button Text</div>;
-};
+// src/index.tsx
+import Button from "./components/Button.js";
 export {
   Button
 };
