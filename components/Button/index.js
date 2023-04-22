0cdd79b58ee4e55a2f04b72a797d59303ff363c9
Moniet <moniet.sawhney@gmail.com>
2023-04-22 17:24:08 +0200

    Updates

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,14 +1,45 @@
-// src/components/Button/index.tsx
-var styles = {
-  padding: "1rem",
-  borderRadius: "10px",
-  background: "#222",
-  color: "#f2f2f2"
-};
-var Button = () => {
-  return <div style={styles}>Button Text</div>;
-};
-var Button_default = Button;
-export {
-  Button_default as default
-};
+"use strict";
+(() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+  }) : x)(function(x) {
+    if (typeof require !== "undefined")
+      return require.apply(this, arguments);
+    throw new Error('Dynamic require of "' + x + '" is not supported');
+  });
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+    mod
+  ));
+
+  // src/components/Button/index.tsx
+  var React = __toESM(__require("react"));
+  var styles = {
+    padding: "1rem",
+    borderRadius: "10px",
+    background: "#222",
+    color: "#f2f2f2"
+  };
+  var Button = () => {
+    return <div style={styles}>Button Text</div>;
+  };
+  var Button_default = Button;
+})();
