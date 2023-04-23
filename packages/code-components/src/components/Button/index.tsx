8575947eb331c6360b21d9b1542b4ac6830a1aa0
Moniet <moniet.sawhney@gmail.com>
2023-04-23 08:42:56 +0200

    🐞 fix tsc failing

--- packages/code-components/src/components/Button/index.tsx
+++ packages/code-components/src/components/Button/index.tsx
@@ -1,5 +1,5 @@
-import styles from "./Button.modules.css"
+import styles from "./Button.modules.css";
 
 export const Button = () => {
-  return <div className={styles.btn}>Button Text</div>
-}
+  return <div className={styles.btn}>Button Text</div>;
+};
