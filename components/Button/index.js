0ac483035d7c0469b70e787c8611a99f46a595b9
Moniet <moniet.sawhney@gmail.com>
2023-04-28 19:59:19 +0200

    Updates

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,4 +1,4 @@
-var e="721303a715e881505d41a171c4426c3a2c9fff68e9e1ce9640cd86a286e1a06b",i=`._btn_1ek6r_1 {
+var e="26a36bde64da91d48424284c23907580b56723a5c74d169f8c267f9ff4ee9ac4",i=`._btn_1ek6r_1 {
   background: #222;
   color: white;
   border-radius: 10px;
@@ -6,4 +6,4 @@ var e="721303a715e881505d41a171c4426c3a2c9fff68e9e1ce9640cd86a286e1a06b",i=`._bt
   font-family: Helvetica, sans-serif;
   width: fit-content;
 }
-`;(function(){if(globalThis.window&&!document.getElementById(e)){var t=document.createElement("style");t.id=e,t.textContent=i,document.head.appendChild(t)}})();var o={btn:"_btn_1ek6r_1"};import{useStripeState as r}from"../../hooks/useStripeState.js";import{jsx as a}from"react/jsx-runtime";var f=({label:t="Button"})=>{let[c,n]=r();return a("div",{className:o.btn,onClick:()=>n("item"),children:t})};export{f as Button};
+`;(function(){if(globalThis.window&&!document.getElementById(e)){var t=document.createElement("style");t.id=e,t.textContent=i,document.head.appendChild(t)}})();var o={btn:"_btn_1ek6r_1"};import{useStripeState as r}from"../../hooks/useStripeState.js";import{jsx as d}from"react/jsx-runtime";var m=({label:t="Button"})=>{let[a,n]=r();return d("div",{className:o.btn,onClick:()=>n("item"),children:t})};export{m as Button};
