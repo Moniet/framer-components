893aac9d462cc00b93d94fcc6bc9a29ca34c9913
Moniet <moniet.sawhney@gmail.com>
2023-04-29 16:35:44 +0200

    Updates

--- components/ScaleImage/index.js
+++ components/ScaleImage/index.js
@@ -1,19 +1,19 @@
-import{ControlType as e}from"framer";var r="50915761890087c6fa4d5313f141c9c75cb8a49158e01fc62972bc3b27860ebb",m=`._img_q3doa_1 {
+import{ControlType as e}from"framer";var i="710eb947482436ebed828ed0392b09b5aa5033abdfaec9f8f8d93148e1ec21d3",m=`._img_otic9_1 {
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   transition: transform var(--dur, 0.3s) var(--easing);
-  transform: scale(1.2);
+  transform: scale(1);
 }
 
-._container_q3doa_9 {
+._container_otic9_9 {
   width: var(--width, 200px);
   height: var(--height, 200px);
   border-radius: var(--br, 10px);
   overflow: hidden;
 }
 
-._container_q3doa_9:hover > ._img_q3doa_1 {
+._container_otic9_9:hover ._img_otic9_1 {
   transform: scale(var(--scale, 1.2));
 }
-`;(function(){if(globalThis.window&&!document.getElementById(r)){var t=document.createElement("style");t.id=r,t.textContent=m,document.head.appendChild(t)}})();var a={img:"_img_q3doa_1",container:"_container_q3doa_9"};import{jsx as i}from"react/jsx-runtime";var s="https://labs.moniet.dev/framer-components/public/france.jpeg",_=({image:t,alt:o="",easing:n="ease",dur:d=.3,width:p=200,height:l=200,br:u=10,scale:c=1.2})=>i("div",{className:a.container,style:{"--dur":d+"s","--easing":n,"--br":u+"px","--height":l+"px","--width":p+"px","--scale":c},children:i("img",{className:a.img,src:t?.src||s,alt:o})}),b={image:{type:e.ResponsiveImage,defaultValue:s},radius:{type:e.Number,defaultValue:10},dur:{title:"Duration",type:e.Number,default:.3,min:.1,max:2,step:.1,displayStepper:!0},width:{type:e.Number,displayStepper:!0,step:10,defaultValue:200},height:{type:e.Number,displayStepper:!0,step:10,defaultValue:200},easing:{type:e.Enum,options:["ease","ease-in-out","ease-out","ease-in","linear"],optionTitles:["Ease","Ease-In-Out","Ease-Out","Ease-In","Linear"],defaultValue:"ease"}};export{_ as ScaleImage,b as propsControls};
+`;(function(){if(globalThis.window&&!document.getElementById(i)){var t=document.createElement("style");t.id=i,t.textContent=m,document.head.appendChild(t)}})();var a={img:"_img_otic9_1",container:"_container_otic9_9"};import{jsx as r}from"react/jsx-runtime";var s="https://labs.moniet.dev/framer-components/public/france.jpeg",_=({image:t,alt:o="",easing:n="ease",dur:d=.3,width:p=200,height:l=200,br:c=10,scale:u=1.2})=>r("div",{className:a.container,style:{"--dur":d+"s","--easing":n,"--br":c+"px","--height":l+"px","--width":p+"px","--scale":u},children:r("img",{className:a.img,src:t?.src||s,alt:o})}),b={image:{type:e.ResponsiveImage,defaultValue:s},radius:{type:e.Number,defaultValue:10},dur:{title:"Duration",type:e.Number,default:.3,min:.1,max:2,step:.1,displayStepper:!0},width:{type:e.Number,displayStepper:!0,step:10,defaultValue:200},height:{type:e.Number,displayStepper:!0,step:10,defaultValue:200},easing:{type:e.Enum,options:["ease","ease-in-out","ease-out","ease-in","linear"],optionTitles:["Ease","Ease-In-Out","Ease-Out","Ease-In","Linear"],defaultValue:"ease"}};export{_ as ScaleImage,b as propsControls};
--- components/StripeProductCard/index.js
+++ components/StripeProductCard/index.js
@@ -1,4 +1,4 @@
-var o="c1685a8ba02fc57b9ddb7abb504bfb9f61260ee9e6942f13017df3abf500ece9",p=`._container_1fkre_1 {
+var o="f4234193c7a9611ab7944d9d87cd93878c54bf26a247c1cdde26aa5a2cfb4db0",p=`._container_1fkre_1 {
   font-size: 16px;
   position: relative;
   font-family: inherit;
@@ -33,4 +33,4 @@ var o="c1685a8ba02fc57b9ddb7abb504bfb9f61260ee9e6942f13017df3abf500ece9",p=`._co
   height: 50%;
   object-fit: cover;
 }
-`;(function(){if(globalThis.window&&!document.getElementById(o)){var e=document.createElement("style");e.id=o,e.textContent=p,document.head.appendChild(e)}})();var t={container:"_container_1fkre_1",card:"_card_1fkre_10",selectedView:"_selectedView_1fkre_19",image:"_image_1fkre_31"};import{useStripeState as g}from"../../hooks/useStripeState.js";import{ControlType as c,addPropertyControls as _}from"framer";import{jsx as i,jsxs as s}from"react/jsx-runtime";var h=({productId:e,productTitle:a})=>{let[n,l,f]=g(),d=n.find(r=>r.id===e),m=()=>{d?f({id:e}):l({id:e,title:a})};return s("div",{className:t.container,onClick:r=>{r.preventDefault(),m()},children:[s("div",{className:t.card,children:[i("img",{className:t.image,src:"https://placehold.co/600x400.png",alt:""}),i("div",{className:t.title,children:i("b",{children:a})})]}),d&&i("div",{className:t.selectedView})]})};_(h,{productId:{type:c.String},productTitle:{type:c.String}});export{h as StripeProductCard};
+`;(function(){if(globalThis.window&&!document.getElementById(o)){var e=document.createElement("style");e.id=o,e.textContent=p,document.head.appendChild(e)}})();var t={container:"_container_1fkre_1",card:"_card_1fkre_10",selectedView:"_selectedView_1fkre_19",image:"_image_1fkre_31"};import{useStripeState as g}from"../../hooks/useStripeState.js";import{ControlType as c,addPropertyControls as _}from"framer";import{jsx as i,jsxs as s}from"react/jsx-runtime";var h=({productId:e,productTitle:r})=>{let[n,l,f]=g(),a=n.find(d=>d.id===e),m=()=>{a?f({id:e}):l({id:e,title:r})};return s("div",{className:t.container,onClick:d=>{d.preventDefault(),m()},children:[s("div",{className:t.card,children:[i("img",{className:t.image,src:"https://placehold.co/600x400.png",alt:""}),i("div",{className:t.title,children:i("b",{children:r})})]}),a&&i("div",{className:t.selectedView})]})};_(h,{productId:{type:c.String},productTitle:{type:c.String}});export{h as StripeProductCard};
