7e8049cc695035a4957e1d65de1619bd7c63473b
Moniet <moniet.sawhney@gmail.com>
2023-04-30 14:03:53 +0200

    📝 goo component

---
+++ packages/code-components/src/components/Goo/DefaultGoo.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.css"
+
+const Dot = ({ from, to }) => {
+  return (
+    <div
+      style={
+        {
+          "--from": from,
+          "--to": to
+        } as any
+      }
+      className={styles.dot}
+    />
+  )
+}
+
+export const DefaultGoo = () => {
+  return (
+    <div className={styles.defaultContainer}>
+      <Dot from="0px" to={"-25px"} />
+      <Dot from="0px" to={"25px"} />
+    </div>
+  )
+}
---
+++ packages/code-components/src/components/Goo/index.tsx
@@ -0,0 +1,53 @@
+import * as React from "react"
+import styles from "./styles.module.css"
+import { ControlType } from "framer"
+import { DefaultGoo } from "./DefaultGoo"
+
+type Props = {
+  children?: React.ReactNode
+  br?: number
+  id?: string
+}
+
+export const Goo = ({ children, br = 0, id = "123" }: Props) => {
+  const filterId = `fc-goo-${id}`
+
+  return (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <filter id={filterId}>
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              type="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+              result="goo"
+            />
+            <feBlend in="SourceGraphic" in2="goo" />
+          </filter>
+        </defs>
+      </svg>
+      <div
+        className={styles.children}
+        style={{ filter: `url(#${filterId})` } as any}
+      >
+        {children || <DefaultGoo />}
+      </div>
+    </>
+  )
+}
+
+export const propsControls = {
+  children: {
+    type: ControlType.Array,
+    controls: {
+      type: ControlType.ComponentInstance
+    }
+  },
+  br: {}
+}
