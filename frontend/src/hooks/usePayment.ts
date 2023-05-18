1102e253e5cdfdecc8205472866fa4ebb5e353da
Moniet <moniet.sawhney@gmail.com>
2023-05-18 19:58:18 +0200

    ✨ usePayment fix

--- frontend/src/hooks/usePayment.ts
+++ frontend/src/hooks/usePayment.ts
@@ -1,3 +1,4 @@
+import { useUser } from '@supabase/auth-helpers-react'
 import { useComponents } from './useComponents'
 import { useRouter } from 'next/router'
 
@@ -5,8 +6,11 @@ export const usePayment = () => {
   const { data, isLoading } = useComponents()
   const hasPaid = !!data
   const router = useRouter()
+  const user = useUser()
 
   const handleRedirect = () => {
+    if (isLoading) return null
+    if (!hasPaid && user) return router.push('/purchase')
     if (hasPaid) return router.push('/demo')
     router.push('/sign-in?payment=true')
   }
