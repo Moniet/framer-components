f8bc5254dc8c53bd393d9f1526522f4584dd3fb2
Moniet <moniet.sawhney@gmail.com>
2023-05-18 16:49:59 +0200

    update build command

--- frontend/src/utils/stripePrices.ts
+++ frontend/src/utils/stripePrices.ts
@@ -1,6 +1,6 @@
-const isLocal = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production'
+const isLocal = process.env.NEXT_PUBLIC_VERCEL_ENV === 'local'
 
 export const priceIds = {
-  lifetime: isLocal ? 'price_1N8isQCMVxcjybDEegEf3LMk' : '',
+  lifetime: isLocal ? 'price_1N8isQCMVxcjybDEegEf3LMk' : 'price_1N97n6CMVxcjybDEXBAWvqgj',
   yearlyUpdates: '',
 }
