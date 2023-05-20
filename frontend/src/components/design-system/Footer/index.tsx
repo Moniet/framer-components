22b948eaba6acd3e07c61cdd6d766bdafacee150
Moniet <moniet.sawhney@gmail.com>
2023-05-20 10:38:37 +0200

    ✨ footer social links

--- frontend/src/components/design-system/Footer/index.tsx
+++ frontend/src/components/design-system/Footer/index.tsx
@@ -1,9 +1,28 @@
+import { Flex } from 'theme-ui'
+
 const Footer = () => {
   return (
-    <div sx={{ display: 'flex', width: '100%', padding: '3rem', mt: '5rem' }}>
+    <div
+      sx={{
+        display: 'flex',
+        width: '100%',
+        padding: '3rem',
+        mt: '5rem',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
       <div>
         <p>© FramerLib. 2023</p>
       </div>
+      <Flex sx={{ gap: '2rem', fontWeight: 600 }}>
+        <a referrerPolicy="no-referrer" href="https://twitter.com/framerlib">
+          Twitter
+        </a>
+        <a referrerPolicy="no-referrer" href="mailto:support@framerlib.com">
+          Contact
+        </a>
+      </Flex>
     </div>
   )
 }
